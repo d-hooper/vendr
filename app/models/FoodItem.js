e5f17bc740a300cae107cf56f7781183d9822187
d-hooper <dallinhoop3@gmail.com>
2025-02-25 15:06:33 -0700

    additional styling, changed imgs to img tags in html template and added buttons for buying items

--- app/models/FoodItem.js
+++ app/models/FoodItem.js
@@ -10,10 +10,18 @@ export class FoodItem {
   get itemCardHTMLTemplate() {
     return `
     <div class="col-4">
-      <div class="mt-3 card ${this.name} d-flex justify-content-end food-item-card">
-        <div class="bg-dark text-light text-capitalize d-flex justify-content-between py-3 px-2 fs-4">
-          <p class="my-0">${this.name}</p>
-          <span>$${this.price.toFixed(2)}</span>
+      <div class="mt-3 card ${this.name} rounded-3 d-flex justify-content-end food-item-card">
+        <div>
+          <img src="${this.image}">
+        </div>
+        <div class="bg-dark text-light text-capitalize d-flex justify-content-between align-items-center py-3 px-2 fs-6">
+          <div>
+            <p class="my-0">${this.name}</p>
+            <span>$${this.price.toFixed(2)}</span>
+          </div>
+          <div>
+            <button class="btn btn-warning fw-bold text-light text-shadow" role="button" title="Click to buy a ${this.name}">Buy</button>
+          </div>
         </div>
       </div>
     </div>
